56dcc554ffd2cc4b8f15c13b619c1e6f1d53d494
deonii <fishingman99@naver.com>
2023-03-14 13:53:42 +0900

    project start

---
+++ src/main/java/deonii/mybox/MyboxApplication.java
@@ -0,0 +1,15 @@
+package deonii.mybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class MyboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboxApplication.class, args);
+	}
+
+}
